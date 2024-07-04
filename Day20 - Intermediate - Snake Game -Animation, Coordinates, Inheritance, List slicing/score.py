b1d9aa8bf1834c6ce981ca09f8f413a46fa29cbd
Anish <anishupreti2002@gmail.com>
2024-07-04 22:04:41 +0545

    first commit

---
+++ Day20 - Intermediate - Snake Game -Animation, Coordinates, Inheritance, List slicing/score.py
@@ -0,0 +1,25 @@
+from turtle import Turtle
+
+ALIGNMENT = "center"
+FONT = ("Arial", 15, "normal")
+
+
+class Score(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.hideturtle()
+        self.penup()
+        self.goto(0, 320)
+        self.color("white")
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.color("red")
+        self.write("Game Over", align=ALIGNMENT, font=FONT)
